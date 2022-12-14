25f023ab48d50dde7fac7c33646574bca2045489
andreresende36 <andreresende36@gmail.com>
2022-12-14 16:53:03 -0300

    Feature 6: done

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
   state = {
     searchName: '',
     disableBtn: true,
+    isLoading: false,
+    artist: '',
+    albumArray: [],
+    result: '',
   };
 
   handleChange = ({ target: { name, value } }) => {
@@ -15,11 +22,44 @@ export default class Search extends Component {
     });
   };
 
+  handleClick = () => {
+    const { searchName } = this.state;
+    this.setState({ isLoading: true });
+    searchAlbumsAPI(searchName)
+      .then((response) => {
+        this.setState({
+          searchName: '',
+          isLoading: false,
+          artist: searchName,
+          albumArray: response,
+        }, () => {
+          const { artist, albumArray } = this.state;
+          this.setState({
+            result: albumArray.length === 0
+              ? 'Nenhum álbum foi encontrado'
+              : `Resultado de álbuns de: ${artist}`,
+          });
+        });
+      });
+  };
+
   render() {
-    const { state: { searchName, disableBtn }, handleChange } = this;
+    const {
+      state: { searchName,
+        disableBtn,
+        isLoading,
+        albumArray,
+        result,
+      },
+      handleChange,
+      handleClick,
+    } = this;
     return (
       <div data-testid="page-search">
+        {/* Cabeçalho */}
         <Header />
+
+        {/* Formulário */}
         <input
           type="text"
           data-testid="search-artist-input"
@@ -33,9 +73,35 @@ export default class Search extends Component {
           type="button"
           data-testid="search-artist-button"
           disabled={ disableBtn }
+          onClick={ handleClick }
         >
           Pesquisar
         </button>
+
+        {/* Mostrar o componente Loading */}
+        { isLoading
+          ? (<Loading isLoading={ isLoading } />)
+          : null }
+        <br />
+
+        {/* Mostrar resultados da busca */}
+        <p>{ result }</p>
+
+        {/* Container de Cards de Álbuns */}
+        <div className="albumCardsContainer">
+          {albumArray.map((album) => (
+            <div key={ album.collectionId } className="albumCard">
+              <img src={ album.artworkUrl100 } alt="imagem do álbum" />
+              <Link
+                to={ `/album/${album.collectionId}` }
+                data-testid={ `link-to-album-${album.collectionId}` }
+              >
+                {album.collectionName}
+              </Link>
+              <p>{album.artistName}</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
